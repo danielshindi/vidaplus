82248a75ef2320ed6a69d3087a85e52bbc158605
danielshindi <danielshindi@hotmail.com>
2025-07-05 12:22:45 -0300

    Implementação de urls

--- vidaplus/urls.py
+++ vidaplus/urls.py
@@ -15,8 +15,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from usuarios.views import UsuarioViewSet, PerfilUsuarioViewSet
+from pacientes.views import PacienteViewSet
+from profissionais.views import ProfissionalSaudeViewSet
+from agendamentos.views import ConsultaViewSet
+from telemedicina.views import TeleconsultaViewSet
+from internacoes.views import InternacaoViewSet, LeitoViewSet
+from receitas.views import ReceitaViewSet
+from prontuarios.views import ProntuarioViewSet
+from notificacoes.views import NotificacaoViewSet
+from relatorios.views import RelatorioViewSet
+from auditoria.views import LogEntryViewSet
+
+router = DefaultRouter()
+router.register(r'usuarios', UsuarioViewSet)
+router.register(r'perfis', PerfilUsuarioViewSet)
+router.register(r'pacientes', PacienteViewSet)
+router.register(r'profissionais', ProfissionalSaudeViewSet)
+router.register(r'consultas', ConsultaViewSet)
+router.register(r'teleconsultas', TeleconsultaViewSet)
+router.register(r'internacoes', InternacaoViewSet)
+router.register(r'leitos', LeitoViewSet)
+router.register(r'receitas', ReceitaViewSet)
+router.register(r'prontuarios', ProntuarioViewSet)
+router.register(r'notificacoes', NotificacaoViewSet)
+router.register(r'relatorios', RelatorioViewSet)
+router.register(r'logs', LogEntryViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
